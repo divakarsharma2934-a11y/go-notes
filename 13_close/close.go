812c0ded561dd26d17ad94a67247eb9582619fb5
Divakar <divakarsharma2934@gmail.com>
2026-01-11 16:10:02 +0530

    closeur

--- 13_close/close.go
+++ 13_close/close.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+func counter() func() int {
+	var count = 0
+	return func() int {
+		count += 1
+		return count
+	}
+
+}
+
 func main() {
+	intt := counter()
+	fmt.Println(intt())
+	fmt.Println(intt())
 
 }
