207f8180b28fbb37e5cb7d22d9e578f9d550ee17
Divakar <divakarsharma2934@gmail.com>
2026-01-13 16:58:34 +0530

    waitgroups

--- 19_Goroutines/rout.go
+++ 19_Goroutines/rout.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func run(i int) {
+func run(i int, w *sync.WaitGroup) {
+	defer w.Done()
 	fmt.Println(i)
 }
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 0; i <= 10; i++ {
-		//go run(i)
-		go func(i int) {
-			fmt.Println(i)
-		}(i)
+		wg.Add(1)
+		go run(i, &wg)
+		//go func(i int) {
+		//	fmt.Println(i)
+		//}(i)
 	}
-	time.Sleep(1 * time.Second)
+	//time.Sleep(1 * time.Second)
+	wg.Wait()
 
 }
